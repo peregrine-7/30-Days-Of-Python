d47324d66cb25bc5d68a858c9603100bd96c8ded
Christopher Lu <christopherlu@Christophers-MacBook-Air.local>
2026-06-30 22:58:50 -0400

    Day 3 complete

---
+++ 03_Day_Operators/day_3/day_3_exercises.py
@@ -0,0 +1,34 @@
+age = 19
+height = 1.8
+complex = 1 + 1j
+
+# base = input("Enter Base: ")
+# height = input("Enter height: ")
+# print("The area of the triangle is ", int(base) * int(height) / 2)
+
+print(len('python'))
+print(len('dragon'))
+print('python' is not 'dragon')
+print('on' in 'python' and 'on' in 'dragon')
+print('jargon' in 'I hope this course is not full of jargon.')
+print(str(float(len('python'))))
+
+print(7 // 3 == int(2.7))
+
+print(type('10') == type(10))
+# print(int('9.8') == 10)
+
+print(int(2.5))
+print(int(2.499999))
+print(int(2.9999999999))
+print(int(3.0))
+
+print("""
+1 1 1 1 1
+2 1 2 4 8
+3 1 3 9 27
+4 1 4 16 64
+5 1 5 25 125
+      """)
+
+
