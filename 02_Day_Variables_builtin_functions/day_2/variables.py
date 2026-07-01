9fdfcb283ee2ad40c1b621ef20d312c2a4f3c86f
Christopher Lu <christopherlu@Christophers-MacBook-Air.local>
2026-06-30 22:44:41 -0400

    Day 2 completed

---
+++ 02_Day_Variables_builtin_functions/day_2/variables.py
@@ -0,0 +1,27 @@
+# Day 2: 30 days of python programming
+first_name = 'Christopher'
+last_name = 'Lu'
+full_name = 'Christopher Lu'
+
+age, height, city = 19, 6, 'Pittsburgh'
+
+print(type(age))
+print(type(city))
+print(len(first_name))
+
+num_one, num_two = 5, 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+print(type(division))
+print(type(floor_division))
+
+area_of_circle = 30 **2 * 3.14
+circum_of_circle = 2 * 30 * 3.14
+
+radius = input("input radius: ")
+print(int(radius) ** 2 * 3.14)
